9c4fddec840f2120cadcefd471c902588edd2068
Spoodry <jan080599xd@gmail.com>
2020-01-28 20:56:40 -0600

    acomodo

--- almacen/conexion.php
+++ almacen/conexion.php
@@ -1,5 +1,5 @@
 <?php
     function Conectar() {
-        return mysqli_connect('localhost','chronoxme','chr0n0xm3.std.2019','chronoxm_sistema');
+        return mysqli_connect('chronox.me','chronoxme','chr0n0xm3.std.2019','chronoxm_sistema');
     }
 ?>
